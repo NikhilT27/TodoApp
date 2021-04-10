66c4b915c611879370deb4c69b8420f9d99dfb3c
NikhilT27 <nikhilthakare14@gmail.com>
2021-04-10 19:00:33 +0530

    add: basic functionality

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Pressable, Image } from "react-native";
 import { FlatList, ScrollView, TextInput } from "react-native-gesture-handler";
 import EachTask from "./components/EachTask";
 
@@ -10,18 +10,43 @@ export default function App() {
   const renderItem = ({ item }) => {
     return (
       <View>
-        <EachTask item={item}></EachTask>
+        <EachTask
+          handleEditButton={handleEditButton}
+          handleDeleteButton={handleDeleteButton}
+          item={item}
+        ></EachTask>
       </View>
     );
   };
 
-  function handleEditButton(data) {
-    let index = tasks.findIndex(data);
+  function handleEditButton(id, text) {
+    console.log(id);
+    console.log(text);
+
+    let newTasks = tasks.map((each) => {
+      if (text != "") {
+        if (each.id === id) {
+          return { id: id, task: text };
+        }
+      }
+
+      return each;
+    });
+
+    setTasks([...newTasks]);
+  }
+
+  function handleDeleteButton(id) {
+    let newTasks = tasks.filter((each) => each.id !== id);
+    setTasks([...newTasks]);
   }
 
   function onAddButton() {
     if (text !== "") {
-      setTasks([...tasks, text]);
+      let id =
+        text.split(" ").join("") + Math.floor(Math.random() * 10000).toString();
+
+      setTasks([...tasks, { id: id, task: text }]);
       setText("");
     }
   }
@@ -32,25 +57,34 @@ export default function App() {
         <Text style={styles.appTitle}>TODO APP</Text>
         <Text style={styles.appSub}>Add New Task !!</Text>
       </View>
-      <View style={styles.addTaskBox}>
-        <TextInput
-          style={styles.taskInput}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => setText(text)}
-          value={text}
-        ></TextInput>
-        <Button
+      <View style={styles.addTaskBoxRelative}>
+        <View style={styles.addTaskBox}>
+          <View style={styles.taskInputBox}>
+            <Text style={styles.taskInputTitle}>Add Task</Text>
+            <TextInput
+              style={styles.taskInput}
+              placeholder="Enter Task"
+              onChangeText={(text) => setText(text)}
+              value={text}
+            ></TextInput>
+          </View>
+          <Pressable style={styles.addButton} onPress={onAddButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </Pressable>
+          {/* <Button
           onPress={onAddButton}
-          title="Add Task"
-          color="#841584"
+          title="+"
+          color="red"
           accessibilityLabel="Add Task"
-        />
+        /> */}
+        </View>
       </View>
       <View style={styles.storedTaskbox}>
+        <Text style={styles.tasksTitle}>TASKS</Text>
         <FlatList
           data={tasks}
           renderItem={renderItem}
-          keyExtractor={(item) => item}
+          keyExtractor={(item) => item.id}
         ></FlatList>
       </View>
     </View>
@@ -60,49 +94,100 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
+    position: "relative",
     // backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 
   appTitleBox: {
-    // flex: 3,
     paddingTop: 100,
+    paddingBottom: 100,
     width: "100%",
-    backgroundColor: "yellow",
+    backgroundColor: "#0095ff",
     alignItems: "center",
     justifyContent: "flex-start",
   },
 
-  addTaskBox: {
-    // flex: 1,
-    padding: 20,
+  taskInputBox: {
     width: "100%",
-    backgroundColor: "red",
+  },
+
+  taskInputTitle: {
+    fontSize: 16,
+    fontWeight: "700",
+    marginBottom: 10,
+  },
+
+  addTaskBoxRelative: {
+    marginBottom: -60,
+  },
+
+  addTaskBox: {
+    top: -60,
+    alignSelf: "center",
+    flexDirection: "row",
+    padding: 20,
+    width: "80%",
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+
+  addButton: {
+    width: 50,
+    height: 50,
     alignItems: "center",
     justifyContent: "center",
+    borderRadius: 50,
+    backgroundColor: "#0095ff",
+    textAlign: "center",
   },
+
+  addButtonText: {
+    textAlign: "center",
+    height: 40,
+    fontSize: 30,
+    color: "white",
+    fontWeight: "200",
+  },
+
   taskInput: {
-    padding: 10,
-    width: 200,
-    height: 50,
-    backgroundColor: "white",
+    width: "100%",
+    padding: 20,
+    paddingTop: 0,
+    fontSize: 24,
   },
 
   storedTaskbox: {
-    // flex: 4,
+    flex: 4,
     alignItems: "center",
     width: "100%",
   },
 
   appTitle: {
-    fontSize: 30,
+    fontSize: 40,
     fontWeight: "700",
+    color: "white",
   },
 
   appSub: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: "400",
+    color: "white",
+  },
+
+  tasksTitle: {
+    marginLeft: "5%",
+    marginTop: "5%",
+    fontSize: 20,
+    fontWeight: "700",
+    alignSelf: "flex-start",
   },
 });
--- components/EachTask.js
+++ components/EachTask.js
@@ -1,14 +1,73 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
-export default function EachTask({ item }) {
+export default function EachTask({
+  item,
+  handleEditButton,
+  handleDeleteButton,
+}) {
+  const [editText, setEditText] = useState("");
+  const [storeText, setStoreText] = useState("");
+  const [openEdit, setOpenEdit] = useState(false);
+
+  useEffect(() => {
+    setStoreText(item.task);
+  }, []);
+
+  function onEditButtonPress() {
+    if (editText != "") {
+      handleEditButton(item.id, editText);
+      setEditText("");
+    }
+  }
+
   return (
-    <View style={styles.eachTaskBox}>
-      <Text>{item}</Text>
-      <View style={styles.options}>
-        <Button title="Edit" />
-        <Button title="Delete" />
+    <View>
+      <View style={styles.eachTaskBox}>
+        {/* <Text>{item.id}</Text> */}
+        <Text>{item.task}</Text>
+        {/* <TextInput
+          defaultValue={item.task}
+          // value={storeText}
+          onChangeText={(text) => setEditText(text)}
+        /> */}
+
+        <View style={styles.options}>
+          <Button
+            onPress={() => setOpenEdit(!openEdit)}
+            title="Edit"
+            color="#841584"
+            accessibilityLabel="Edit Task"
+          />
+          <Button
+            onPress={() => handleDeleteButton(item.id)}
+            title="Delete"
+            color="#841584"
+            accessibilityLabel="Delete Task"
+          />
+        </View>
       </View>
+      {openEdit ? (
+        <View style={styles.editTaskBox}>
+          <Text>Edit Task</Text>
+          <TextInput
+            defaultValue={editText}
+            // value={storeText}
+            placeholder="Edit task"
+            onChangeText={(text) => setEditText(text)}
+            onSubmitEditing={onEditButtonPress}
+          />
+          <Button
+            onPress={onEditButtonPress}
+            title="Ok"
+            color="#841584"
+            accessibilityLabel="Edit Task"
+          />
+        </View>
+      ) : (
+        <></>
+      )}
     </View>
   );
 }
@@ -20,6 +79,16 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-evenly",
+  },
+
+  editTaskBox: {
+    alignSelf: "flex-end",
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 20,
+    marginRight: 20,
   },
 
   options: {
