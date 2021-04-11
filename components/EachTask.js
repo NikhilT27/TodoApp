62b7570dfb9fdb387bfaf334888535633563f32e
NikhilT27 <nikhilthakare14@gmail.com>
2021-04-11 09:37:32 +0530

    add: extra marginRight to bullet

--- components/EachTask.js
+++ components/EachTask.js
@@ -89,6 +89,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#0095ff",
     borderRadius: 10,
     marginTop: 10,
+    marginRight: 10,
   },
   eachTaskBoxMain: {
     marginBottom: 10,
